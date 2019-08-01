5f129cca9a42b1760eb0aa1f1e454a9cde5c0de6
matthewjones <matthewjones2435@gmail.com>
2019-08-01 13:52:24 -0600

    add BaseControllerTest class

---
+++ src/test/java/io/github/processthis/springserver/controller/SketchControllerTest.java
@@ -0,0 +1,12 @@
+package io.github.processthis.springserver.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+
+class SketchControllerTest {
+
+  @BeforeEach
+  void setUp() {
+  }
+}
