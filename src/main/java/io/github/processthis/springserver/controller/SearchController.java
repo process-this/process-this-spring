5a5421f5bb6e67abe3ff2b8fa0294340eb486c21
Monjoie11 <jeff.franken@gmail.com>
2019-08-06 13:36:59 -0600

    two classes of java docs  added

--- src/main/java/io/github/processthis/springserver/controller/SearchController.java
+++ src/main/java/io/github/processthis/springserver/controller/SearchController.java
@@ -13,16 +13,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * This is the controller class for queries providing information to the home and featured views of
+ * the android app.
+ */
 @RestController
 @RequestMapping("search")
 public class SearchController {
 
-    private final SketchRepository sketchRepository;
-    private final UserProfileRepository userProfileRepository;
-    private final LikeRepository likeRepository;
+  private final SketchRepository sketchRepository;
+  private final UserProfileRepository userProfileRepository;
+  private final LikeRepository likeRepository;
 
 
-
+  /**
+   * This constructor is used to create an instance of the SearchController class using the three
+   * parameters listed below.
+   */
   @Autowired
   public SearchController(
       SketchRepository repository,
@@ -33,16 +40,34 @@ public class SearchController {
     this.likeRepository = likeRepository;
   }
 
-  @GetMapping( produces = MediaType.APPLICATION_JSON_VALUE)
+  /**
+   * This method searches for all Sketches containing a given String in their name attribute and
+   * returns them as a list
+   *
+   * @param fragment One or more characters of a search term
+   * @return A list of Sketch objects
+   */
+  @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
   public List<Sketch> search(@RequestParam("q") String fragment) {
     return sketchRepository.findAllByNameContainingOrderByNameAsc(fragment);
   }
 
+  /**
+   * This method searches for a returns a list of sketch objects ordered by the most Likes in
+   * descending order
+   *
+   * @param count is the number of sketches to return, with 10 being default
+   */
   @GetMapping(value = "feed", produces = MediaType.APPLICATION_JSON_VALUE)
-  public List<Sketch> get(@RequestParam(value = "count", required = false, defaultValue = "10") int count) {
+  public List<Sketch> get(
+      @RequestParam(value = "count", required = false, defaultValue = "10") int count) {
     return sketchRepository.getMostLikedSketches(count);
   }
 
+  /**
+   * This method searches for and returns the 10 most recently created sketch objects in descending
+   * order
+   */
   @GetMapping(value = "home", produces = MediaType.APPLICATION_JSON_VALUE)
   public List<Sketch> getHome() {
     return sketchRepository.getTop10ByOrderByCreatedDesc();
