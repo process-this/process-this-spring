1abf645ba1814d04c2eca41fd5c14090802f15e2
matthewjones <matthewjones2435@gmail.com>
2019-08-07 00:23:30 -0600

    begin testing methods for controllers

--- src/test/java/io/github/processthis/springserver/BaseControllerTest.java
+++ src/test/java/io/github/processthis/springserver/BaseControllerTest.java
@@ -3,6 +3,8 @@ package io.github.processthis.springserver;
 import static capital.scalable.restdocs.misc.AuthorizationSnippet.documentAuthorization;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
@@ -10,12 +12,15 @@ import static org.springframework.restdocs.operation.preprocess.Preprocessors.pr
 import capital.scalable.restdocs.AutoDocumentation;
 import capital.scalable.restdocs.jackson.JacksonResultHandlers;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.github.processthis.springserver.model.entity.UserProfile;
 import java.util.Base64;
 import java.util.Random;
 import org.junit.jupiter.api.BeforeEach;
+import org.springframework.http.MediaType;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.http.HttpDocumentation;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.RequestPostProcessor;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
@@ -29,12 +34,13 @@ public class BaseControllerTest {
   private MockMvc mockMvc;
   private final ObjectMapper mapper;
   private final WebApplicationContext context;
-  private final Random rng;
+  private final Random rng = new Random();
 
-  protected  BaseControllerTest(ObjectMapper mapper, WebApplicationContext context, Random rng) {
+  protected  BaseControllerTest(ObjectMapper mapper, WebApplicationContext context) {
     this.mapper = mapper;
     this.context = context;
-    this.rng = rng;
+
+
   }
 
   @BeforeEach
@@ -45,7 +51,7 @@ public class BaseControllerTest {
             documentationConfiguration(provider)
             .uris()
             .withScheme("http")
-            .withHost("host")// FIXME need to find out where we will host and use the address here.
+            .withHost("http://localhost:18181")// FIXME need to find out where we will host and use the address here.
             .withPort(443)
             .and()
             .snippets()
@@ -89,5 +95,24 @@ public class BaseControllerTest {
       return documentAuthorization(request, OAUTH_NOTICE);
     // TODO find out bearer and authorization info
     };
+  }
+
+  public ResultActions addUser(String username, String authId) throws Exception {
+    return getMockMvc().perform(
+        post("/users")
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON)
+            .with(oauthTokenRequired())
+            .content(String.format("{\"username\": \"%s\", \"authId\":\"%s\"}", username, authId))
+    );
+  }
+
+  public   ResultActions addAndAttribute(String username, String auth, StringBuilder builder) throws Exception {
+    builder.append("/users/");
+    addUser(username, auth).andDo(mvcResult -> {
+      UserProfile userProfile = getMapper().readValue(mvcResult.getResponse().getContentAsString(),UserProfile.class);
+      builder.append(userProfile.getId().toString());
+    });
+    return getMockMvc().perform(get(builder.toString()).with(oauthTokenRequired()));
   }
 }
